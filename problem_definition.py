049efe9a271454565b670fbd36eae49bbdf846c4
Ivano Matrisciano <ivanomatrisciano@gmail.com>
2025-05-07 18:44:09 +0200

    Refactor problem_definition.py

--- problem_definition.py
+++ problem_definition.py
@@ -5,98 +5,139 @@ import os
 from planner import Planner
 
 class ProblemDefinition():
-    '''Class that contains all methods to manage input data'''
+    """Class that contains all methods to manage input data"""
     def __init__(self):
+        self.event = None
+        self.liquid = None
+        self.output_path = None
+        self.problem_path = None
+        self.problem = None
+        self.objective = None
+        self.available_methods = ["METADATA", "OGAMUS"]
+        self.available_actions = [
+            "Move Agent",
+            "Pickup Object",
+            "Open Object",
+            "Close Object",
+            "Break Object",
+            "Cook Object",
+            "Slice Object",
+            "Toggle On Object",
+            "Toggle Off Object",
+            "Dirty Object",
+            "Clean Object",
+            "Fill Object",
+            "Empty Object",
+            "Use Up Object",
+            "Drop Object (Requires holding an object)",
+            "Put Object (Requires holding an object)"]
+
+        self.available_scenes = {
+            "kitchens": range(1,30+1),
+            "living_rooms": range(201, 230+1),
+            "bedrooms": range(301, 330+1),
+            "bathrooms": range(401, 430+1)
+        }
+
         self.problem_list = []
         self.objective_list = []
+        self.method = None
+        self.scene_number = None
+
+    @staticmethod
+    def _user_choice_in_list(items: list, message: str, print_list: bool = False) -> tuple:
+        """Asks the user tho make a choice in a given items list, returning both the index and the item"""
+        if print_list:
+            for index, item in enumerate(items):
+                print(f"[{index}] {item}")
+        while True:
+            index_str = input(message)
+            if index_str.isdigit():
+                index = int(index_str)
+                if 0 <= index < len(items):
+                    return index, items[index]
 
     def method_selection(self):
         '''Method that allows selecting execution type:
         1. METADATA: Uses data returned by simulator to locate objects and agent positions
         2. OGAMUS: Uses OGAMUS algorithm to scan scenes using pretrained neural networks'''
-        bucle = True
-        while bucle:
-            print("----METHOD----")
-            print("[1] - METADATA")
-            print("[2] - OGAMUS")
-            print("--------------")
-            self.method = int(input("Select the method used to solve the problem: "))
-            print("")
-            if 1 <= self.method <= 2:
-                bucle = False
-                self.method = str(self.method)
-            else:
-                print("Please, input a valid option\n")
 
+        chosen_method, _ = self._user_choice_in_list(self.available_methods, "Select the method used to solve the problem: ", print_list=True)
+        chosen_method += 1  # because methods are "1" or "2" ...
+        chosen_method = str(chosen_method)
+        self.set_method(chosen_method)
         return self.method
+
+    def set_method(self, method):
+        self.method = method
         
 
     def scene_selection(self):
-        '''Method that allows selection of the scene'''
-        bucle = True
-        while bucle:
+        """Method that allows selection of the scene"""
+        invalid_input = True
+        while invalid_input:
             print("----SCENE----")
             print("[1-30] - Kitchens")
             print("[201-230] - Living Rooms")
             print("[301-330] - Bedrooms")
             print("[401-430] - Bathrooms")
             print("---------------")
-            self.scene_number = int(input("Select a scene: "))
-            print("")
-            if (1 <= self.scene_number <= 30) or (201 <= self.scene_number <= 230) or (301 <= self.scene_number <= 330) or (401 <= self.scene_number <= 430):
-                bucle = False
-                self.scene_number = str(self.scene_number)
-            else:
-                print("Please, input a valid scene\n")
+
+            user_input = input("Select a scene: ")
+            if not user_input.isdigit():
+                continue
+
+            user_number = int(user_input)
+            for scene_category in self.available_scenes:
+                if user_number in self.available_scenes[scene_category]:
+                    invalid_input = False
+                    self.scene_number = str(user_number)
 
         return self.scene_number
 
+    def set_scene(self, scene_number):
+        self.scene_number = str(scene_number)
+
     def paths_selection(self, iteracion):
-        '''Method that specifies problem and output paths'''
+        """Method that specifies problem and output paths"""
         self.problem_path = f'./pddl/problems/problem{iteracion}.pddl'
         self.output_path = f'./pddl/outputs/problem{iteracion}.txt'
 
         return self.problem_path, self.output_path
 
     def problem_selection(self, event):
-        '''Method that allows selecting problems and objectives'''
+        """Method that allows selecting problems and objectives"""
         self.liquid = 'coffee'
         self.event = event
-        bucle = True
-        holding = False
-        while bucle:
-            print("----ACTION----")
-            print("[1] - Move Agent")
-            print("[2] - Pickup Object")
-            print("[3] - Open Object")
-            print("[4] - Close Object")
-            print("[5] - Break Object")
-            print("[6] - Cook Object")
-            print("[7] - Slice Object")
-            print("[8] - Toggle On Object")
-            print("[9] - Toggle Off Object")
-            print("[10] - Dirty Object")
-            print("[11] - Clean Object")
-            print("[12] - Fill Object")
-            print("[13] - Empty Object")
-            print("[14] - Use Up Object")
-            print("[15] - Drop Object (Requires holding an object)")
-            print("[16] - Put Object (Requires holding an object)")
-            print("----------------")
-            aux = input("Select an action: ")
-            print("")
-            for obj in event.metadata['objects']:
-                if obj['isPickedUp']:
-                    holding = True
-            if (1 <= int(aux) <= 14):
-                bucle = False
-            elif (15 <= int(aux) <= 16) and holding:
-                bucle = False
+        invalid_input = True
+
+        chosen_action, chosen_action_name = self._user_choice_in_list(self.available_actions, "Select an action: ", print_list=True)
+        aux = chosen_action + 1
+
+        while invalid_input:
+            holding = any(x["isPickedUp"] for x in event.metadata["objects"])
+
+            if aux != "" and (1 <= int(aux) <= 14):
+                invalid_input = False
+            elif aux != "" and (15 <= int(aux) <= 16) and holding:
+                invalid_input = False
             else:
                 print("Please, input a valid action\n")
+                chosen_action, chosen_action_name = self._user_choice_in_list(self.available_actions, "Select an action: ")
+                aux = chosen_action + 1
 
+        print(f"Chosen action: {chosen_action_name}")
+        return self.set_problem(event, aux)
 
+    def set_problem(self, event, problem_number: str):
         # Establecer parámetros en caso de que se seleccione problema de movimiento
+
+        if (problem_number is str and not problem_number.isdigit()) \
+                or int(problem_number) < 0 \
+                or int(problem_number) > 15:
+            raise ValueError("problem_number must be a string containing a number between 0 and 15")  # I know...
+
+        aux = int(problem_number) + 1
         if str(aux) == '1':
             self.problem = "move"
             print("----OBJECTIVE----")
@@ -152,34 +193,41 @@ class ProblemDefinition():
             self.object_selection("useup", "canBeUsedUp", "isUsedUp", False)
         
         if str(aux) == '15':
+            holding = any(x["isPickedUp"] for x in event.metadata["objects"])
+            if not holding:
+                raise Exception("Action 'drop' requires holding an item")
             self.object_selection("drop", "pickupable", "isPickedUp", True)
         
         if str(aux) == '16':
+            holding = any(x["isPickedUp"] for x in event.metadata["objects"])
+            if not holding:
+                raise Exception("Action 'put' requires holding an item")
             self.object_selection("put", "receptacle", "receptacle", True)
-
             
         return self.problem, self.objective, self.liquid
 
+
+    def _get_objects(self, condition1, condition2, condition2_res, select_liquid=False):
+        """Given the specified problem type and conditions it retuns a list of objects currently in scene that meet thos criteria"""
+        return [x for x in self.event.metadata["objects"] if x[condition1] and x[condition2] == condition2_res]
+
     def object_selection(self, problem_type, condition1, condition2, condition2_res, select_liquid=False):
-        '''Method that contains the objective selection of most of the problems'''
+        """Method that contains the objective selection of most of the problems"""
         self.problem = problem_type
-        posible_objects = []
-        for obj in self.event.metadata["objects"]:
-            if (obj[condition1] == True) and (obj[condition2] == condition2_res):
-                posible_objects.append(obj)
-        if not posible_objects:
+        possible_objects = self._get_objects(condition1, condition2, condition2_res)
+
+        if not possible_objects or len(possible_objects) == 0:
             print("There aren't any objectives for this action. Select another or switch the scene\n")
             return self.problem_selection(self.event)
-        print("----OBJECTIVE----")         
-        i = 0
-        for obj in posible_objects:
+
+        print("----OBJECTIVE----")
+        for (i, obj) in enumerate(possible_objects):
             print(f'[{i}] - {obj["objectId"]}')
-            i += 1
         print("----------------")
-        aux2 = input("Select the objective: ")
-        self.objective = posible_objects[int(aux2)]            
-        print("")
-        print(f'Selected objective is: {self.objective["objectId"]} {self.objective["name"]}\n')
+
+        # Ask the user to choose an object
+        _, self.objective = self._user_choice_in_list(possible_objects, "Select the objective: ")
+        print(f'\nSelected objective is: {self.objective["objectId"]} {self.objective["name"]}\n')
 
         if select_liquid:
             print("----LIQUID----")
@@ -196,10 +244,12 @@ class ProblemDefinition():
             elif str(inp) == '3':
                 self.liquid = 'water'
 
+        return None
+
     def problem_selection_ogamus(self):
-        '''Method that allows user to select problems to be solved with OGAMUS'''
-        bucle = True
-        while bucle:
+        """Method that allows user to select problems to be solved with OGAMUS"""
+        invalid_input = True
+        while invalid_input:
             print("----ACTION----")
             print("[1] - Get Close To Object")
             print("[2] - Pickup Object")
@@ -221,12 +271,12 @@ class ProblemDefinition():
             aux = input("Select an action: ")
             print("")
             if (1 <= int(aux) <= 16):
-                bucle = False
+                invalid_input = False
             else:
                 print("Please, input a valid action\n")
         
-        problemas_ogamus = ["get_close_to", "pickup", "open", "close", "break", "cook", "slice", "toggleon", "toggleoff", "dirty", "clean", "fill", "empty", "useup", "drop", "put"]
-        self.problem = problemas_ogamus[int(aux)-1]
+        problems_ogamus = ["get_close_to", "pickup", "open", "close", "break", "cook", "slice", "toggleon", "toggleoff", "dirty", "clean", "fill", "empty", "useup", "drop", "put"]
+        self.problem = problems_ogamus[int(aux) - 1]
         self.problem_list.append(self.problem)
 
         self.object_selection_ogamus()
